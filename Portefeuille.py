29a05e4197e9c10d49e1b8582b557c1b52974bdb
Luckny Yvon Buissereth <lybui1@ulaval.ca>
2023-12-03 14:15:00 -0500

    ajout arriver a la methode acheter

--- Portefeuille.py
+++ Portefeuille.py
@@ -6,24 +6,24 @@ class Portefeuille:
         self.transactions = []
 
     
-def déposer(self, montant, date=date.today()):
-    if date > date.today():
-        raise  TimeoutError
-    self.liquiditer = montant
-    self.transactions.append({"type": "dépôt", "montant": montant, "date": date})
+    def déposer(self, montant, date=date.today()):
+        if date > date.today():
+            raise  TimeoutError
+        self.liquiditer = montant
+        self.transactions.append({"type": "dépôt", "montant": montant, "date": date})
 
 
     
-def solde(self,date=date.today):
-    if date > date.today():
-        raise  TimeoutError
-    solde = 0
-    for transaction in self.transactions :
-        if transaction["date"] <= date:
-            if transaction["type"] == "dépôt":
-                solde += transaction["montant"]
-            elif transaction["type"] == "vente":
-                solde += transaction["montant_liquide"]
+    def solde(self,date=date.today):
+        if date > date.today():
+            raise  TimeoutError
+        solde = 0
+        for transaction in self.transactions :
+            if transaction["date"] <= date:
+                if transaction["type"] == "dépôt":
+                    solde += transaction["montant"]
+                elif transaction["type"] == "vente":
+                    solde += transaction["montant_liquide"]
 
         return solde
     
@@ -40,4 +40,17 @@ def solde(self,date=date.today):
         self.transactions.append({"type": "dépôt", "montant": -coût_total, "date": date})
 
 
-    def vendre(self,symbole, quantité, date):
+    def vendre(self,symbole, quantité, date=date.today()):
+        if date > date.today():
+            raise  TimeoutError
+       
+        self.prix_unit = self.bourse.prix(symbole, date)
+        quantité_en_portefeuille = self.quantité_titres(symbole, date)
+
+        if quantité > quantité_en_portefeuille:
+            raise ErreurQuantité("Quantité insuffisante de titres pour effectuer la vente.")
+
+        montant_vente = self.prix_unitaire * quantité
+
+        self.transactions.append({"type": "vente", "symbole": symbole, "quantité": quantité, "date": date})
+        self.transactions.append({"type": "dépôt", "montant": montant_vente, "date": date})
