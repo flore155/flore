0b9b1085ec3ebe8c4dd866a684c10279c64087c0
Luckny Yvon Buissereth <lybui1@ulaval.ca>
2023-12-04 09:00:01 -0500

    correction de mon datetime et ajout de la methode valeur totale

--- Portefeuille.py
+++ Portefeuille.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import datetime
 
 class Portefeuille:
     def __init__(self, bourse):
@@ -6,7 +6,7 @@ class Portefeuille:
         self.transactions = []
 
     
-    def déposer(self, montant, date=date.today()):
+    def déposer(self, montant, date=datetime.now().date()):
         if date > date.today():
             raise  TimeoutError
         self.liquiditer = montant
@@ -14,7 +14,7 @@ class Portefeuille:
 
 
     
-    def solde(self,date=date.today):
+    def solde(self,date=datetime.now().date()):
         if date > date.today():
             raise  TimeoutError
         solde = 0
@@ -27,11 +27,11 @@ class Portefeuille:
 
         return solde
     
-    def acheter(self,symbole, quantité, date=date.today):
+    def acheter(self,symbole, quantité, date=datetime.now().date()):
         if date > date.today():
             raise  TimeoutError
-        self.prix_unit = self.bourse.prix(symbole, date)
-        coût_total = self.prix_unit * quantité
+        prix_unit = self.bourse.prix(symbole, date)
+        coût_total = prix_unit * quantité
         solde_actuel = self.solde(date) 
         
         if solde_actuel < coût_total:
@@ -40,17 +40,26 @@ class Portefeuille:
         self.transactions.append({"type": "dépôt", "montant": -coût_total, "date": date})
 
 
-    def vendre(self,symbole, quantité, date=date.today()):
+    def vendre(self,symbole, quantité, date=datetime.now().date()):
         if date > date.today():
             raise  TimeoutError
        
-        self.prix_unit = self.bourse.prix(symbole, date)
+        prix_unit = self.bourse.prix(symbole, date=datetime.now().date())
         quantité_en_portefeuille = self.quantité_titres(symbole, date)
 
         if quantité > quantité_en_portefeuille:
             raise ErreurQuantité("Quantité insuffisante de titres pour effectuer la vente.")
 
-        montant_vente = self.prix_unitaire * quantité
+        montant_vente = prix_unit * quantité
 
         self.transactions.append({"type": "vente", "symbole": symbole, "quantité": quantité, "date": date})
         self.transactions.append({"type": "dépôt", "montant": montant_vente, "date": date})
+    
+    def valeur_totale(self, date=datetime.now().date()):
+        if date > date.today():
+            raise  TimeoutError
+        
+        solde_liquide = self.solde(date)
+        valeur_titres = sum(self.bourse.prix(trans["symbole"], date) * trans["quantité"]
+                            for trans in self.transactions if trans["type"] == "achat")
+        return solde_liquide + valeur_titres
