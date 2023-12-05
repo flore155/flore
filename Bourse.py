71798c0a514c226b479b7268eccbd7b7f5e8982a
Luckny Yvon Buissereth <lybui1@ulaval.ca>
2023-12-04 21:47:29 -0500

    correction de mon code pas de correction de peps et du code de Flore

--- Bourse.py
+++ Bourse.py
@@ -4,31 +4,32 @@ import requests
 import json
 
 class Bourse:
-    def prix(self,symbole,_date):
-        self.symbole = symbole 
-        self.date = _date
+    def prix(self, symbole, _date):
         url = f'https://pax.ulaval.ca/action/{symbole}/historique/'
-        réponse = requests.get(url=url)#, params=params)
+        réponse = requests.get(url=url)
         réponse = json.loads(réponse.text)
-        historique = réponse['historique']
-        historique = dict(reversed(sorted(historique.items())))
-        date_en_cours = datetime.today()
-        date_en_cours = date_en_cours.strftime('%Y-%m-%d')
-        date_en_cours_obj = datetime.strptime(date_en_cours,'%Y-%m-%d')
-        _date_str = _date.strftime('%Y-%m-%d')
-        if _date>date_en_cours_obj:
-            raise ErreurDate("Erreur date")
-        elif historique.get(_date_str)!=None:
-            valeur = historique[_date_str]['fermeture']
-        #elif _date_obj == date_en_cours_obj:
-         #   valeur = historique[_date]['fermeture']
-        else:
-            for dat_historique in réponse['historique'].keys():
-                dat_historique_obj = datetime.strptime(dat_historique, '%Y-%m-%d')
-                if dat_historique_obj < _date:
-                    valeur = historique[dat_historique]['fermeture']
-                    break
-        
-        return valeur
 
-#print(Bourse().prix('goog', datetime.strptime('2023-12-04','%Y-%m-%d')))
+        historique = réponse.get('historique', {})
+        historique = dict(reversed(sorted(historique.items())))
+
+        date_en_cours = datetime.today().date()
+        _date_str = _date.strftime('%Y-%m-%d')
+
+        if _date > date_en_cours:
+            raise ErreurDate("Erreur date")
+
+        valeur = None
+
+        if _date_str in historique:
+            valeur = historique[_date_str]['fermeture']
+        else:
+            for dat_historique, valeurs in historique.items():
+                dat_historique_obj = datetime.strptime(dat_historique, '%Y-%m-%d').date()
+                if dat_historique_obj <= _date:
+                    valeur = valeurs['fermeture']
+                    break
+
+        if valeur is None:
+            raise ErreurDate("Aucune valeur trouvée pour la date spécifiée")
+
+        return valeur
